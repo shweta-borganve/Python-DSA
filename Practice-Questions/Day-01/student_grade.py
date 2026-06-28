0eeb6b6bb673250b74931d5ff29901d186cebbff
shweta <shweta.borganve@gmail.com>
2026-06-29 00:25:02 +0530

    Added student_grade.py it calculates the student's grade in the class

---
+++ Practice-Questions/Day-01/student_grade.py
@@ -0,0 +1,14 @@
+grade = [85, 92, 78, 96, 88]
+
+grade.append(90) 
+
+average_grade = sum(grade) / len(grade)
+print("Average grade:", average_grade)
+
+highest_grade = max(grade)
+print("Highest grade:", highest_grade) 
+
+lowest_grade = min(grade)
+print("Lowest grade:", lowest_grade) 
+
+print(grade) 
