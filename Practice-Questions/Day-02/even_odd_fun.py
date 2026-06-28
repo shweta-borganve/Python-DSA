72d36c32b02caef5124cbe7a5d2e7afbcbef2f72
shweta <shweta.borganve@gmail.com>
2026-06-29 00:58:01 +0530

    Added even_odd_fun.py it gives even and odd number using the function

---
+++ Practice-Questions/Day-02/even_odd_fun.py
@@ -0,0 +1,8 @@
+def even_odd(num):
+    if num % 2 == 0:
+        print("The entered number is Even") 
+    else:
+        print("The entered number is Odd") 
+
+    num = int(input("Enter a number: ")) 
+    even_odd(num) 
