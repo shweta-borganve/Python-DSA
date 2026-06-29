8e41f18fe43215df9421aa8675bcf030690c13c9
shweta <shweta.borganve@gmail.com>
2026-06-29 15:00:29 +0530

    modified palindrome_check.py

--- Practice-Questions/Day-02/palindrome_check.py
+++ Practice-Questions/Day-02/palindrome_check.py
@@ -3,4 +3,4 @@ text = input("Enter a string: ")
 if text == text[::-1]:
     print("The string is a palindrome.")
 else:
-    print("The string is not a palindrome.") 
+    print("The string is not a palindrome.")  
