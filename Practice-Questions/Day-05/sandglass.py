bf1db8cedf72e1755cb841d78b7834d348ea7d2c
shweta <shweta.borganve@gmail.com>
2026-07-02 21:16:36 +0530

    Added right_angle_triangle.py

--- Practice-Questions/Day-05/sandglass.py
+++ Practice-Questions/Day-05/sandglass.py
@@ -1,18 +1,16 @@
 def generate_right_angle_triangle(n):
     result = []
-
     for i in range(n):
         spaces = " " * i
-        stars = "*" * (2 * (n-i) - 1)
+        stars = "*" * (2*(n-i)-1) 
         result.append(spaces + stars + spaces) 
 
     for i in range(1, n):
         spaces = " " * (n-i-1)
-        stars = "*" * (2*i + 1)
+        stars = "*" * (2*i+1)
         result.append(spaces + stars + spaces) 
 
-    return result
+    return result 
 result = generate_right_angle_triangle(5)
-for row in result:
+for row in result:  
     print(row) 
-         
