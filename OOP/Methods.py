b21146c30dc67c7575a2969743717aaa4560563f
shweta <shweta.borganve@gmail.com>
2026-07-09 00:10:07 +0530

    added /Methods.py

---
+++ OOP/Methods.py
@@ -0,0 +1,15 @@
+class Student:
+    college_name = "ABC College" 
+
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+
+    def welcome(self):
+        print("welcome student", self.name) 
+
+    def get_marks(self):
+        return self.marks
+s1 = Student("karan", 87)
+s1.welcome()
+print(s1.get_marks()) 
