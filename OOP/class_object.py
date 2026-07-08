65d8003853bbc38dd788d9667b733a32ca251129
shweta <shweta.borganve@gmail.com>
2026-07-08 22:37:14 +0530

    It defines the class and object

---
+++ OOP/class_object.py
@@ -0,0 +1,15 @@
+class Student:
+    name = "Karan" 
+    marks = 78
+
+s1 = Student()
+print(s1.name) 
+print(s1.marks) 
+
+class Car:
+    color = "white"
+    brand = "Mercedes"
+
+car1 = Car()
+print(car1.color) 
+print(car1.brand) 
