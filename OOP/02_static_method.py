d34424b25a29f06da4513ee3c2c784b66c1b4cc9
shweta <shweta.borganve@gmail.com>
2026-07-09 21:43:03 +0530

    added 02_static_method.py

---
+++ OOP/02_static_method.py
@@ -0,0 +1,13 @@
+class Calculator:
+    
+    @staticmethod
+    def add(a,b):
+        print(a+b) 
+
+Calculator.add(10,20) 
+
+class Student:
+    def welcome():
+        print("Welcome to OOPs concept") 
+
+Student.welcome() 
