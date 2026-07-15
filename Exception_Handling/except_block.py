6aef25e881ada70038dc663764f0415759e013c7
shweta <shweta.borganve@gmail.com>
2026-07-15 14:51:04 +0530

    Added  except_block.py the except block handles the exception if an error occurs inside the try block

---
+++ Exception_Handling/except_block.py
@@ -0,0 +1,14 @@
+try:
+    a = 20
+    b = 0
+    print(a/b)
+except:
+    print("cannot divide by 0")
+print("program end") 
+
+try:
+    age = int(input("Enter your age: "))
+    print("Age: ", age)
+except:
+    print("Please enter a valid number.")
+print("program end") 
